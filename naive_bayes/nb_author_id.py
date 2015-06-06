f10855ab1723122197ee82b8fec7eb72697546de
accavdar <cetin@udemy.com>
2015-06-06 17:17:40 +0300

    naive bayes mini-project is completed

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,34 +1,37 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 
-""" 
-    this is the code to accompany the Lesson 1 (Naive Bayes) mini-project 
+"""
+    this is the code to accompany the Lesson 1 (Naive Bayes) mini-project
 
     use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
-### your code goes here ###
-
-
+classifier = GaussianNB()
+t_fit = time()
+classifier.fit(features_train, labels_train)
+print "training time: ", round(time() - t_fit, 3), "s"
+t_predict = time()
+predict = classifier.predict(features_test)
+print "predict time: ", round(time() - t_predict, 3), "s"
+print accuracy_score(predict, labels_test)
 #########################################################
 
 
