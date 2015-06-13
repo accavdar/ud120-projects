8702d78eb486161ea0d1150b4bc344382ea25c3d
accavdar <cetin@udemy.com>
2015-06-13 16:43:19 +0300

    decision tree mini-project is completed

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 3 (decision tree) mini-project
 
     use an DT to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,8 +26,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+classifier = DecisionTreeClassifier(min_samples_split=40)
+classifier.fit(features_train, labels_train)
+predict = classifier.predict(features_test)
+print "accuracy:", accuracy_score(predict, labels_test)
+print "features count:", len(features_train[0])
 #########################################################
 
 
